0dce19c453261c8cfbbeb878fe61ece16f569bf7
Arjan Wooning <arjan@wooning.cz>
2021-04-09 20:00:14 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 MQTT_ServerIP     = "192.168.5.248"
 MQTT_ServerPort   = 1883
 # DHCP client list name: GW-XXXXXXXXXXXX
-TRADFRI_HUB_IP    = '192.168.5.101'
+TRADFRI_HUB_IP    = '192.168.5.129'
 LOG_FILENAME      = "/home/pi/log/tradfri_mqtt.log"
 CONFIG_FILE       = 'tradfri_standalone_psk.conf'
 
